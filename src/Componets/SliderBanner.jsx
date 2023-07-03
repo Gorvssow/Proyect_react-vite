bd671405d2083bdf57a8d0346be7b3a204813795
Gregorio Alfonso Rosales Vela <arosales@todoensubastas.com.mx>
2023-07-03 17:56:49 -0600

    feat: modificacion de componente slider

--- src/Componets/SliderBanner.jsx
+++ src/Componets/SliderBanner.jsx
@@ -17,10 +17,10 @@ const SliderBanner = () => {
                 <img src={banner1} alt="Banner 1" style={{ width: '100%', height: 'auto' }}/>
             </div>
             <div>
-                <img src={banner2} alt="Banner 1" style={{ width: '100%', height: 'auto' }}/>
+                <img src={banner2} alt="Banner 2" style={{ width: '100%', height: 'auto' }}/>
             </div>
             <div>
-                <img src={banner3} alt="Banner 1" style={{ width: '100%', height: 'auto' }}/>
+                <img src={banner3} alt="Banner 3" style={{ width: '100%', height: 'auto' }}/>
             </div>
         </Slider>
     );
